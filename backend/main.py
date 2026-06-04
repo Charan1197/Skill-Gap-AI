689a1766464220c1d1df129817d1312868c87a66
Charan1197 <kolapallicharan@gmail.com>
2026-06-04 13:15:15 +0530

    updated CORS in backend

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:5173"],
+    allow_origins = [
+        "http://localhost:5173", 
+        "https://skill-gap-ai-rho.vercel.app"
+    ],
     allow_methods = ["*"],
     allow_headers = ["*"]
 )
